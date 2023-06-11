11d666857434c95e5370ac756b2117f8a42af807
Peter V <98245483+p5quared@users.noreply.github.com>
2023-06-11 11:49:55 -0600

    remove old search function. -add const

--- src/routes/(app)/results/searchForSimilar.js
+++ src/routes/(app)/results/searchForSimilar.js
@@ -1,27 +1,5 @@
-export function searchForSimilar(all_data, start, duration){
-    // Get the target period, and comparable periods of equal duration, and normalize n/sum
-    let target_period = all_data.slice(start, start + duration)
-    target_period = target_period.map((value) => value / target_period.reduce((a, b) => a + b, 0))
-    const before = chunkify(all_data.slice(0, start), duration)
-    const after = chunkify(all_data.slice(start+duration), duration)
-    let comparable_periods = before.concat(after)
-    comparable_periods= comparable_periods.map((period) => {
-        return period.map((value) => (value / period.reduce((a, b) => a + b, 0)))
-    })
-
-
-    // {score: n, data: [n1, n2, n3, ...]}
-    let kl_compare = comparable_periods.map((period) => {
-        return {score: kl_divergence(target_period, period), data: period}
-    })
-    kl_compare = kl_compare.sort((a, b) => a.score - b.score)
-    kl_compare = kl_compare.map((period) => period.data) // don't really care about the score anymore
-    return {p: target_period,
-        kl: kl_compare}
-}
-
 // Server side version; updated
-export function searchForSimilarSS(searchData, start, duration){
+export function searchForSimilar(searchData, start, duration){
     /*
 searchData = {
     p: {name: "stock", data: [{date, c1}, ...]},
@@ -29,8 +7,8 @@ searchData = {
      */
     // Get the target period, and comparable periods of equal duration, and normalize n/sum
     let {p, q} = searchData
-    let target_slice = p.data.slice(start, start + duration)
-    let target_period = target_slice.map((value) => value.close / target_slice.reduce((a, b) => a + b.close, 0))
+    const target_slice = p.data.slice(start, start + duration)
+    const target_period = target_slice.map((value) => value.close / target_slice.reduce((a, b) => a + b.close, 0))
     const target_p_returnable = {name: p.name,
       date: p.data[0].date,
       data: target_period}
