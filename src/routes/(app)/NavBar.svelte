8b5429483d9587b5a13e595c72280ff12f560d6c
Peter Vaiciulis <petervaiciulis@gmail.com>
2023-07-01 18:20:47 -0400

    remove lightswitch
    the lightswitch was a little gimmicky and tbh I have never looked at/designed with light-theme in mind...

--- src/routes/(app)/NavBar.svelte
+++ src/routes/(app)/NavBar.svelte
@@ -1,10 +1,7 @@
 <script>
-    import {AppBar, LightSwitch} from "@skeletonlabs/skeleton";
+    import {AppBar} from "@skeletonlabs/skeleton";
 </script>
 
 <AppBar class="shadow">
     <svelte:fragment slot="lead"><a href="/" class="font-bold gradient-heading"><h2>Chronic Stocks</h2></a></svelte:fragment>
-    <svelte:fragment slot="trail">
-        <LightSwitch/>
-    </svelte:fragment>
 </AppBar>
